e9cfe225b766be25260ce212c452e69321ae0596
wang22691392 <35840138+wang22691392@users.noreply.github.com>
2024-10-12 10:54:28 +0800

    Update xxyh.js

--- xxyh.js
+++ xxyh.js
@@ -4,8 +4,7 @@
 
 [rewrite_local]
 
-https://www.xiaoxiongyouhao.com/api/vip/index.php url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/xiaoxiongyouhao.js
-
+https://www.xiaoxiongyouhao.com/api/vip/index.php url script-response-body https://raw.githubusercontent.com/wang22691392/QuanX/refs/heads/master/xxyh.js
 [mitm] 
 
 hostname = www.xiaoxiongyouhao.com
